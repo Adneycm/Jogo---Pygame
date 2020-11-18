d4c198272ded14ab583a60d1b1c7371ddb76fb8a
ykarosa <ykarosa@al.insper.edu.br>
2020-11-18 11:08:10 -0300

    Loop imagem de fundo e imagem do gato

--- Pygame.py
+++ Pygame.py
@@ -65,3 +65,42 @@ largura_objetos = 50
 altura_objetos = 38
 #objeto_imagem = pygame.image.load('c:/Users/adney/OneDrive/Documentos/1° semestre - Insper/Desing de Software/Imagens Pygame/flyingbird').convert_alpha()
 #objeto_imagem = pygame.transform.scale(objeto_imagem, (largura_objetos,altura_objetos) )
+
+# Inicializando o jogo
+jogo = True
+
+# Tempo para atualização de imagens
+clock = pygame.time.Clock()
+
+while jogo:
+    clock.tick(200) # Grante um FPS de 300Hz
+        # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            jogo = False
+
+        # ----- Gera saídas
+    
+    # Chamando a função blit para que nossas imangens apareção na tela
+    tela.blit(imagem_de_fundo, (0,y_imagem_de_fundo))
+    tela.blit(imagem_de_fundo1, (0,y_imagem_de_fundo1))
+    tela.blit(gato_imagem, (250,300))
+
+    # Mudando as posições da imagem de fundo para que tenhamos a impressão do gato estar caindo
+    y_imagem_de_fundo -= 1
+    y_imagem_de_fundo1 -= 1
+
+    # Condicionais para que as imagens continuem aparecendo mesmo após sair da tela
+    if y_imagem_de_fundo1 <= -altura_tela:
+        y_imagem_de_fundo1= altura_tela
+
+    if y_imagem_de_fundo <= -altura_tela:
+        y_imagem_de_fundo= altura_tela
+
+        # ----- Atualiza estado do jogo
+    pygame.display.update() 
+     # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
